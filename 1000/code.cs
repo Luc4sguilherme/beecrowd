4bd29a19dc74707630858ed29163309ebbd7de61
Luc4sguilherme <lucas.guilherme.matutina@gmail.com>
2022-03-10 16:24:11 -0300

    add problem 1000

---
+++ 1000/code.cs
@@ -0,0 +1,15 @@
+using System; 
+
+class URI {
+
+    static void Main(string[] args) { 
+
+        /**
+         * Escreva a sua solução aqui
+         * Code your solution here
+         * Escriba su solución aquí
+         */
+        Console.WriteLine("Hello World!"); 
+    }
+
+}
---
+++ 1000/code.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * Escreva a sua solução aqui
+ * Code your solution here
+ * Escriba su solución aquí
+ */
+
+echo "Hello World!\n";
+
+?>
