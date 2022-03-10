81c18b26823a611955be3113bdb7e8244c8a11da
Luc4sguilherme <lucas.guilherme.matutina@gmail.com>
2022-03-10 17:36:37 -0300

    add problem 1017

---
+++ 1017/code.js
@@ -0,0 +1,9 @@
+var input = require('fs').readFileSync('/dev/stdin', 'utf8'),
+    lines = input.split('\n'),
+    values = lines.map(value => Number(value));
+
+function calculateSpentFuel(spentTime, averageSpeed, carFuelConsumption) {
+    return (spentTime * averageSpeed) / carFuelConsumption
+}
+
+process.stdout.write(`${calculateSpentFuel(values[0], values[1], 12).toFixed(3)}\n`)
