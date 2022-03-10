01246c280574045f26d78fc167bc17a4e19934a1
Luc4sguilherme <lucas.guilherme.matutina@gmail.com>
2022-03-10 20:37:21 -0300

    add problem 1020

---
+++ 1020/code.js
@@ -0,0 +1,16 @@
+var input = require('fs').readFileSync('/dev/stdin', 'utf8');
+var ageInDays = Number(input)
+
+function calculate(age) {
+    var year = Math.floor(age / 365);
+    age = age % 365;
+    
+    var month = Math.floor(age / 30);
+    age = age % 30;
+    
+    var days = Math.floor(age);
+    
+    return `${year} ano(s)\n${month} mes(es)\n${days} dia(s)`
+}
+
+process.stdout.write(`${calculate(ageInDays)}\n`)
