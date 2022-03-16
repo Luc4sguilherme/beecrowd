7f7450433736b27b19ac266219dfbeeebe0dc735
Luc4sguilherme <lucas.guilherme.matutina@gmail.com>
2022-03-15 21:31:15 -0300

    add problem 1052

---
+++ 1052/code.js
@@ -0,0 +1,39 @@
+var input = require("fs").readFileSync("/dev/stdin", "utf8");
+
+function getMonth(number) {
+    switch (number) {
+        case 1:
+            return "January";
+        case 2:
+            return "February";
+        case 3:
+            return "March";
+        case 4:
+            return "April";
+        case 5:
+            return "May";
+        case 6:
+            return "June";
+        case 7:
+            return "July";
+        case 8:
+            return "August";
+        case 9:
+            return "September";
+        case 10:
+            return "October";
+        case 11:
+            return "November";
+        case 12:
+            return "December";
+    }
+}
+
+function main() {
+    var number = Number(input);
+    var month = getMonth(number);
+
+    process.stdout.write(`${month}\n`);
+}
+
+main();
