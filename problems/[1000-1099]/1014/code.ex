ce2c84178ca5867accc2196a572c62d4e1140e37
Luc4sguilherme <lucas.guilherme.matutina@gmail.com>
2022-08-07 18:39:11 -0300

    add solution to problem 1014 in elixir

---
+++ problems/[1000-1099]/1014/code.ex
@@ -0,0 +1,26 @@
+defmodule Car do
+  def calculate_average_consumption(distance, spent_fuel) do
+    distance / spent_fuel
+  end
+end
+
+defmodule Util do
+  def parse_string_as_float(value) do
+    value |> String.trim() |> Float.parse() |> elem(0)
+  end
+
+  def parse_string_as_integer(value) do
+    value |> String.trim() |> String.to_integer()
+  end
+
+  def parse_mantisse(value, mantisse) do
+    :erlang.float_to_binary(value, decimals: mantisse)
+  end
+end
+
+total_distance = IO.gets("") |> Util.parse_string_as_integer()
+spent_fuel_total = IO.gets("") |> Util.parse_string_as_float()
+
+average_consumption = Car.calculate_average_consumption(total_distance, spent_fuel_total) |> Util.parse_mantisse(3)
+
+IO.puts("#{average_consumption} km/l")
