7d09f9566efa6daf90de00de470f6bfed52525db
Luc4sguilherme <lucas.guilherme.matutina@gmail.com>
2022-08-08 11:17:42 -0300

    add solution to problem 1017 in elixir

---
+++ problems/[1000-1099]/1017/code.ex
@@ -0,0 +1,26 @@
+defmodule Car do
+  def calculate_spent_fuel(spent_time, average_speed, fuel_consumption) do
+    spent_time * average_speed / fuel_consumption
+  end
+end
+
+defmodule Util do
+  def parse_mantisse(value, mantisse) do
+    :erlang.float_to_binary(value, decimals: mantisse)
+  end
+
+  def parse_string_as_integer(value) do
+    value |> String.trim() |> String.to_integer()
+  end
+end
+
+spent_time = IO.gets("") |> Util.parse_string_as_integer()
+average_speed = IO.gets("") |> Util.parse_string_as_integer()
+
+car_fuel_consumption = 12
+
+liters_of_fuel =
+  Car.calculate_spent_fuel(spent_time, average_speed, car_fuel_consumption)
+  |> Util.parse_mantisse(3)
+
+IO.puts("#{liters_of_fuel}")
