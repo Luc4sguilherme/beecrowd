5f9dfdff5832110ab40e5369a3ccf728898cbd77
Luc4sguilherme <lucas.guilherme.matutina@gmail.com>
2022-08-10 00:54:27 -0300

    add solution to problem 1043 in elixir

---
+++ problems/[1000-1099]/1043/code.ex
@@ -0,0 +1,41 @@
+defmodule Triangule do
+  def calculate_perimeter(a, b, c) do
+    a + b + c
+  end
+
+  def is_triangle(a, b, c) do
+    a + b > c && b + c > a && a + c > b
+  end
+end
+
+defmodule Trapezium do
+  def calculate_area(a, b, c) do
+    0.5 * (a + b) * c
+  end
+end
+
+defmodule Util do
+  def parse_string_as_float(value) do
+    value |> String.trim() |> Float.parse() |> elem(0)
+  end
+
+  def parse_mantissa(value, mantissa) do
+    :erlang.float_to_binary(value, decimals: mantissa)
+  end
+end
+
+[a, b, c] =
+  IO.gets("")
+  |> String.trim()
+  |> String.split(" ")
+  |> Enum.map(fn x -> Util.parse_string_as_float(x) end)
+
+is_triangle = Triangule.is_triangle(a, b, c)
+
+if(is_triangle) do
+  triangule_perimeter = Triangule.calculate_perimeter(a, b, c) |> Util.parse_mantissa(1)
+  IO.puts("Perimetro = #{triangule_perimeter}")
+else
+  trapezium_area = Trapezium.calculate_area(a, b, c) |> Util.parse_mantissa(1)
+  IO.puts("Area = #{trapezium_area}")
+end
