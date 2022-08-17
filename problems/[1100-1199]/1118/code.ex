2d2b4be5d26551095a46f9a96529c08f194f5205
Luc4sguilherme <lucas.guilherme.matutina@gmail.com>
2022-08-17 00:37:06 -0300

    add solution to problem 1118 in elixir

---
+++ problems/[1100-1199]/1118/code.ex
@@ -0,0 +1,65 @@
+defmodule Util do
+  def parse_string_as_float(value) do
+    value |> String.trim() |> Float.parse() |> elem(0)
+  end
+
+  def parse_string_as_integer(value) do
+    value |> String.trim() |> String.to_integer()
+  end
+
+  def parse_mantissa(value, mantissa) do
+    :erlang.float_to_binary(value, decimals: mantissa)
+  end
+end
+
+defmodule Program do
+  def init() do
+    valid_scores = get_valid_scores()
+
+    average_score = calculate_average_score(valid_scores) |> Util.parse_mantissa(2)
+
+    IO.puts("media = #{average_score}")
+    recalculate?()
+  end
+
+  def recalculate?() do
+    IO.puts("novo calculo (1-sim 2-nao)")
+
+    input =
+      IO.read(:stdio, :line)
+      |> Util.parse_string_as_integer()
+
+    case input do
+      1 -> init()
+      2 -> nil
+      _ -> recalculate?()
+    end
+  end
+
+  def get_valid_scores() do
+    get_valid_scores([])
+  end
+
+  def get_valid_scores(valid_scores) when length(valid_scores) == 2 do
+    valid_scores
+  end
+
+  def get_valid_scores(valid_scores) do
+    score =
+      IO.read(:stdio, :line)
+      |> Util.parse_string_as_float()
+
+    if(score >= 0 && score <= 10) do
+      get_valid_scores([score | valid_scores])
+    else
+      IO.puts("nota invalida")
+      get_valid_scores(valid_scores)
+    end
+  end
+
+  def calculate_average_score(scores) do
+    Enum.sum(scores) / Enum.count(scores)
+  end
+end
+
+Program.init()
