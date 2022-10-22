dc7d7efe810b4870e8d2745e6e54a518fb7770c3
Luc4sguilherme <lucas.guilherme.matutina@gmail.com>
2022-10-22 13:27:15 -0300

    update javascript problem 1013 solution

--- problems/[1000-1099]/1013/code.js
+++ problems/[1000-1099]/1013/code.js
@@ -2,8 +2,17 @@ var input = require('fs').readFileSync('/dev/stdin', 'utf8');
 var lines = input.split(' ');
 var values = lines.map(value => Number(value))
 
-function getBiggestInteger(integerA, integerB) {
-    return (integerA + integerB + Math.abs(integerA - integerB)) / 2
+function getBiggestInteger(integers) {
+    const integerA = integers.shift();
+    const integerB = integers.shift();
+
+    const biggestInteger = (integerA + integerB + Math.abs(integerA - integerB)) / 2;
+
+    if(integers.length > 0) {
+        return getBiggestInteger([biggestInteger, ...integers]);
+    } else {
+        return biggestInteger;
+    }
 }
 
-process.stdout.write(`${getBiggestInteger(values[2], getBiggestInteger(values[0],values[1]))} eh o maior\n`)
+process.stdout.write(`${getBiggestInteger(values)} eh o maior\n`);
