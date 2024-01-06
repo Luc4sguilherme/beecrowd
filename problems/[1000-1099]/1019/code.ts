a6b53e685f62b51fa0654e5c6623d656bc911322
Luc4sguilherme <lucas.guilherme.matutina@gmail.com>
2024-01-06 23:39:39 +0000

    add solution to problem 1019 in typescript

---
+++ problems/[1000-1099]/1019/code.ts
@@ -0,0 +1,16 @@
+var input = require('fs').readFileSync('/dev/stdin', 'utf8');
+var durationInSeconds = Number(input)
+
+function getFormattedTime(duration: number) {
+    var hours = Math.floor(duration / 3600);
+    duration = duration % 3600;
+    
+    var minutes = Math.floor(duration / 60);
+    duration = duration % 60;
+    
+    var seconds = Math.floor(duration);
+    
+    return `${hours}:${minutes}:${seconds}`
+}
+
+process.stdout.write(`${getFormattedTime(durationInSeconds)}\n`)
