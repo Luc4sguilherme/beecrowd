c028921299805992b9d4a0a5c37122f567f715a2
Luc4sguilherme <lucas.guilherme.matutina@gmail.com>
2024-01-07 18:57:52 +0000

    add solution to problem 1037 in typescript

---
+++ problems/[1000-1099]/1037/code.ts
@@ -0,0 +1,12 @@
+var input = require('fs').readFileSync('/dev/stdin', 'utf8');
+
+function verify(number: number) {
+    if(number >= 0 && number <= 25) return `Intervalo [0,25]`;
+    if(number > 25 && number <= 50) return `Intervalo (25,50]`;
+    if(number > 50 && number <= 75) return `Intervalo (50,75]`;
+    if(number > 75 && number <= 100) return `Intervalo (75,100]`;
+
+    return `Fora de intervalo`;
+}
+
+process.stdout.write(`${verify(input)}\n`)
